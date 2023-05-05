13c466b8e22f797ec35cdc895ea6ecec29e3f11d
Bhalala-Fenil-2002 <bhalala.fenil2002@gmail.com>
2023-05-05 19:31:07 +0530

    add payable

--- contracts/payable-demo.sol
+++ contracts/payable-demo.sol
@@ -4,18 +4,30 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Test{
 
-    address payable user = payable(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
+    address payable user1 = payable(0x617F2E2fD72FD9D5503197092aC168c91465E7f2);
+    address payable user2 = payable(0x17F6AD8Ef982297579C203069C1DbfFE4348c372);
+    address payable user3 = payable(0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678);
 
-    function payEther() public payable
-    {
-        
+    function sendEther() public payable {
+
     }
 
-    function getBalance() public view returns (uint){
+    function getBalance() public view returns(uint){
         return address(this).balance;
     }
 
-    function transferEther() public payable {
-        user.transfer(1 ether);
+    function sendToOne() public payable {
+        user1.transfer(1 ether);
     }
+
+    function sendToTwo() public payable {
+        user2.transfer(1 ether);
+    }
+
+    function sendToThree() public payable {
+        user3.transfer(1 ether);
+    }
+
+
+    
 }
