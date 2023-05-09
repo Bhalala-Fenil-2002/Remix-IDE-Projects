59da5c78e9ce3694ce9eee80bfa6f19ebe8b3b3e
Bhalala-Fenil-2002 <bhalala.fenil2002@gmail.com>
2023-05-09 13:57:20 +0530

    pay ether

--- contracts/payable-prc.sol
+++ contracts/payable-prc.sol
@@ -3,5 +3,20 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract PayableDemo {
+
+    address payable pay1 = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+
+    function payEther() public payable {
+
+    }
+
+    function getBalance() view public returns(uint) {
+        return address(this).balance;
+    }
+
+    function sendEther() public {
+        pay1.transfer(address(this).balance);
+    }
     
+
 }
