c6cdb78a77d12250478e67b562a6e5239ebe3503
Bhalala-Fenil-2002 <bhalala.fenil2002@gmail.com>
2023-05-10 19:51:26 +0530

    msg, require, receive function

--- contracts/muti_payEth.sol
+++ contracts/muti_payEth.sol
@@ -3,19 +3,31 @@
 pragma solidity >= 0.7.0 < 0.9.0;
 
 contract multiPayEther {
-    uint[] public lotteryUser;
-    address payable adminUser = payable(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
+    address payable[] public lotteryUser;
+    // address payable adminUser = payable(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
     uint count;
+    address admin;
 
-    function pushLotteryUser(uint addUser) public{
-        lotteryUser.push(addUser);
+    constructor(){
+        admin = msg.sender;
     }
 
-    function payEther() public payable {
+    function pushLotteryUser(address addUser) public{
+        lotteryUser.push(payable (addUser));
     }
+
+    // function payEther() public payable {
+    //     // msg.sender
+    // }
 
     function getBalance() view public returns(uint) {
+        require(msg.sender == admin, "Not admin");
         return address(this).balance;
+    }
+
+    receive() external payable  {
+        require(msg.value == 2 ether, "Minmum 2 Ehter");
+        lotteryUser.push(payable (msg.sender));
     }
 
 }
