a143d3b7b3741611831223b6fbdf7d2b9258324b
Bhalala-Fenil-2002 <bhalala.fenil2002@gmail.com>
2023-05-31 19:35:08 +0530

    freelancing approve project

--- contracts/freelancing.sol
+++ contracts/freelancing.sol
@@ -4,13 +4,14 @@ pragma solidity >=0.5.0 <= 0.9.0;
 
 contract freelancing{
 
-    enum projectStatus{pendding, approve, proccessing, complete}
+    enum projectStatus{pendding, approved, rejected, completed}
 
     struct clientInfo{
         address p_id;
         string project;
         uint p_amt;
         uint time_limit;
+        projectStatus status;
     }
 
     struct freelancerInfo{
@@ -19,44 +20,49 @@ contract freelancing{
     }
 
     clientInfo[] clicents;
-    mapping (uint => freelancerInfo) user;
+    mapping (address => freelancerInfo) user;
  
-    function client(string memory p_name, uint amount, uint timelimit) public {
-        clientInfo memory clicent = clientInfo({ p_id: msg.sender, project: p_name, p_amt: amount, time_limit: timelimit });
+    function client(string memory p_name, uint amount, uint timelimit) public{
+        clientInfo memory clicent = clientInfo(msg.sender, p_name, amount, timelimit, projectStatus.pendding);
         clicents.push(clicent);
     }
-
-    function TotalProjetc() public view returns(uint){
-        return clicents.length;
-    }
- 
-    function getClicent(uint clicentID) public view returns(address, string memory, uint, uint) {
+    
+    function getClicent(uint clicentID) public view returns(address, string memory, uint, uint, projectStatus){
         return( clicents[clicentID].p_id,
             clicents[clicentID].project,
             clicents[clicentID].p_amt,
-            clicents[clicentID].time_limit
+            clicents[clicentID].time_limit,
+            clicents[clicentID].status
         );
     }  
 
-    function freelancer(uint user_id,uint clicent_id) public payable {
-        require(clicents[clicent_id].p_amt == msg.value, "Payable amount not equl project amount.");
-        user[user_id].pro_id.push(clicent_id);
-        if(user[user_id].pro_amt == 0) {
-            user[user_id].pro_amt = msg.value;    
-        } else {
-            user[user_id].pro_amt += msg.value;
-        }
-        // user[user_id].pro_amt = msg.value;
+    function TotalProjetc() public view returns(uint){
+        return clicents.length;
     } 
 
-    function getBalance() public view returns(uint){
-        return address(this).balance;
-    }
+    function freelancer(uint clicent_id) public payable{
+        require(clicents[clicent_id].p_amt == msg.value, "Payable amount not equl project amount.");
+        user[msg.sender].pro_id.push(clicent_id);
+        if(user[msg.sender].pro_amt == 0) {
+            user[msg.sender].pro_amt = msg.value;    
+        } else {
+            user[msg.sender].pro_amt += msg.value;
+        }
+    } 
 
-    function getFreelancer(uint u_id) public view returns(uint[] memory, uint) {
+    function getFreelancer(address u_id) public view returns(uint[] memory, uint){
+        require(u_id == msg.sender, "Record not found.");
         return ( user[u_id].pro_id,
             user[u_id].pro_amt
         );
+    }
+
+    function projectApprove(uint pId) public{
+        clicents[pId].status = projectStatus.approved;
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
     }
 
 }
