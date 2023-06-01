e40d59a9a5fe911979ed507643f7d649043c0879
Bhalala-Fenil-2002 <bhalala.fenil2002@gmail.com>
2023-06-01 19:02:17 +0530

    freelancing project change enum type

--- contracts/freelancing.sol
+++ contracts/freelancing.sol
@@ -5,34 +5,47 @@ pragma solidity >=0.5.0 <= 0.9.0;
 contract freelancing{
 
     enum projectStatus{pendding, approved, rejected, completed}
-
+    
+    enum ScheduleState {proposal, financed, started, approved, released}
+    
     struct clientInfo{
         address p_id;
-        string project;
+        string shortCode;
+        string description;
         uint p_amt;
         uint time_limit;
-        projectStatus status;
+        uint[] freelancer_id;
+        ScheduleState scheduleState;
+        // string project;
+        // projectStatus status;
+        // uint256 value;
     }
 
     struct freelancerInfo{
+        address lancer_add;
         uint[] pro_id;
         uint pro_amt;
+        ScheduleState scheduleState;
+        // projectStatus pro_status;
     }
 
     clientInfo[] clicents;
-    mapping (address => freelancerInfo) user;
+    mapping (uint => freelancerInfo) user;
+    uint[] client_lancer;
  
-    function client(string memory p_name, uint amount, uint timelimit) public{
-        clientInfo memory clicent = clientInfo(msg.sender, p_name, amount, timelimit, projectStatus.pendding);
+    function client(string memory project, string memory description, uint amount, uint timelimit) public{
+        clientInfo memory clicent = clientInfo(msg.sender, project, description, amount, timelimit, client_lancer, ScheduleState.proposal);
         clicents.push(clicent);
     }
     
-    function getClicent(uint clicentID) public view returns(address, string memory, uint, uint, projectStatus){
+    function getClicent(uint clicentID) public view returns(address, string memory, string memory, uint, uint, uint[] memory, ScheduleState){
         return( clicents[clicentID].p_id,
-            clicents[clicentID].project,
+            clicents[clicentID].shortCode,
+            clicents[clicentID].description,
             clicents[clicentID].p_amt,
             clicents[clicentID].time_limit,
-            clicents[clicentID].status
+            clicents[clicentID].freelancer_id,
+            clicents[clicentID].scheduleState
         );
     }  
 
@@ -40,29 +53,49 @@ contract freelancing{
         return clicents.length;
     } 
 
-    function freelancer(uint clicent_id) public payable{
+    function freelancer(uint clicent_id, uint lancer_id) public payable{
         require(clicents[clicent_id].p_amt == msg.value, "Payable amount not equl project amount.");
-        user[msg.sender].pro_id.push(clicent_id);
-        if(user[msg.sender].pro_amt == 0) {
-            user[msg.sender].pro_amt = msg.value;    
-        } else {
-            user[msg.sender].pro_amt += msg.value;
-        }
+        
+        address payable clicent_add = payable(clicents[clicent_id].p_id); 
+        
+        clicents[clicent_id].freelancer_id.push(lancer_id);
+        
+        user[lancer_id].pro_id.push(clicent_id);
+        
+        user[lancer_id].lancer_add = msg.sender;
+        
+        clicent_add.transfer(msg.value);
+        // if(user[lancer_id].pro_amt == 0) {
+        //     user[lancer_id].pro_amt = msg.value;    
+        // } else {
+        //     user[lancer_id].pro_amt += msg.value;
+        // }
     } 
 
-    function getFreelancer(address u_id) public view returns(uint[] memory, uint){
-        require(u_id == msg.sender, "Record not found.");
+    function getFreelancer(uint u_id) public view returns(uint[] memory, uint, ScheduleState, address){
+        require(user[u_id].lancer_add == msg.sender, "Record not found.");
         return ( user[u_id].pro_id,
-            user[u_id].pro_amt
+            user[u_id].pro_amt,
+            user[u_id].scheduleState,
+            user[u_id].lancer_add
         );
     }
 
-    function projectApprove(uint pId) public{
-        clicents[pId].status = projectStatus.approved;
-    }
+    // function approve(uint pId, uint lancer_id) public{
+    //     clicents[pId].status = projectStatus.approved;
+    //     user[lancer_id].pro_status = projectStatus.approved;
+    // }
 
-    function getBalance() public view returns(uint){
-        return address(this).balance;
-    }
+    // function done(uint pId, uint lancer_id) public {
+    //     clicents[pId].status = projectStatus.completed;
+    //     user[lancer_id].pro_status = projectStatus.completed;
+    //     address payable lan_add = payable(user[lancer_id].lancer_add);
+
+    //     lan_add.transfer(clicents[pId].p_amt + clicents[pId].p_amt);
+    // }
+
+    // function getBalance() public view returns(uint){
+    //     return address(this).balance;
+    // }
 
 }
